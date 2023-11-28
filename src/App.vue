1746bc138b5ee08b5a63d39adad51adbc4735dcd
Jin0811 <15797954826@163.com>
2023-11-28 21:53:38 +0800

    feat: 配置别名

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 	<div>
 		<el-button type="primary" :icon="Edit">13</el-button>
+		<Test />
 	</div>
 </template>
 
 <script setup lang="ts">
 import { Edit } from "@element-plus/icons-vue";
+import Test from "@/components/Test.vue";
 </script>
