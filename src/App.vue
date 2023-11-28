b5acc4e1d3f1a26be41b39014b969d9a5e89897e
Jin0811 <15797954826@163.com>
2023-11-28 21:43:47 +0800

    feat: 集成element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-	<div>App</div>
+	<div>
+		<el-button type="primary" :icon="Edit">13</el-button>
+	</div>
 </template>
+
+<script setup lang="ts">
+import { Edit } from "@element-plus/icons-vue";
+</script>
