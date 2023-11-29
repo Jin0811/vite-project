8f2bf6b16955f5c9f2fd4c382f3c5906b93f8e26
Jin0811 <15797954826@163.com>
2023-11-29 21:51:58 +0800

    feat: 去除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-	<div>
-		<el-button type="primary" :icon="Edit">13</el-button>
-		<Test />
-	</div>
+	<div></div>
 </template>
 
-<script setup lang="ts">
-import { Edit } from "@element-plus/icons-vue";
-import Test from "@/components/Test.vue";
-</script>
+<script setup lang="ts"></script>
